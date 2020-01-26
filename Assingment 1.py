878276d2be2b782bf57c0e41e097b88ad1430eda
Sanket Saharkar <sanketsaharkar@gmail.com>
2020-01-25 16:45:12 -0800

    Added Comments

--- Assingment 1.py
+++ Assingment 1.py
@@ -1,11 +1,15 @@
+#importing numpy and matplotlib
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+
+#building x values form 0 to 2*pi with a step of 0.1
 x = np.arange(0,2*np.pi, 0.1)
 y = np.sin(x)
 z = np.cos(x)
 
+#Creating graph of one period sine and cosine function
 fig = plt.figure(figsize = (10,5))
 ax1 = fig.add_subplot(1,2,1)
 ax2 = fig.add_subplot(1,2,2)
