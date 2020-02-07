36f8db413d7921dd8797daa11ec531cf868ca3d4
Sanket Saharkar <sanketsaharkar@gmail.com>
2020-02-07 00:31:54 -0800

    Update flaskpractice.py

--- flaskpractice.py
+++ flaskpractice.py
@@ -2,10 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route(’/hello/<name>’, methods=[’GET’])
-def hello_world(name):
-    return "Hello, {}!".format(name)
-    
- 
- if __name__ == "__main__":
- app.run(host=’0.0.0.0’, port=8080)
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_world():
+    return "Hello, world!"
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello_name(name):
+    return f"Hello, {name}"
+
+@app.route('/interesting/<name>', methods=['GET'])
+def serve_html(name):
+    return f"""
+    <html>
+    <body>
+    <h1>Hello {name}</h1>
+    <p>This is my fancy webpage</p>
+    </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
